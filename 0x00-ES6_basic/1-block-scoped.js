a7302c75ff6a5338ba01da8846d3b20c50c022f6
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-12-23 11:05:19 +0000

    number 1, using block

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  var task = false;
+  var task2 = true;
 
-  /* eslint-disable no-unused-vars */
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    let task = true;
+    let task2 = false;
   }
-  /* eslint-enable no-unused-vars */
 
   return [task, task2];
 }
